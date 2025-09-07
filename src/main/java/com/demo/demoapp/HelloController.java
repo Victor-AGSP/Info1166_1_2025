f857ec2838d70f53e405e186f60186ffc8f7b965
Victor-AGSP <victorsepulveda.agp@gmail.com>
2025-09-07 19:45:34 -0300

    Base de proyecto

---
+++ src/main/java/com/demo/demoapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.demoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "index";
+    }
+}
